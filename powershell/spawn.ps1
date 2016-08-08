d9d7ee2c351cf7be735a56501130541caa29acdd
Rogan Dawes <rogan@sensepost.com>
2016-08-08 15:04:07 -0700

    Update spawn to properly delay when no io was possible

--- powershell/spawn.ps1
+++ powershell/spawn.ps1
@@ -24,40 +24,40 @@ function connect($device, $stdout, $stdin) {
 	$device_total = 0
 	Write-Output "Entering main loop"
 	$device.Write($empty_buffer, 0, $M+1)
+	$io = $false
 	while ($stdout_task -ne $null -and $device_task -ne $null) {
 		try {
-			if (!$can_write) { [System.Console]::Write("!") }
-			if ($stdout_task.IsCompleted) {
-				if ($can_write) {
-					$stdout_bytes_read = $stdout.EndRead($stdout_task)
-					if ($stdout_bytes_read -gt 0) {
-						$stdout_total += $stdout_bytes_read
-						[string]::Format( "Socket {0} - Device {1}", $stdout_total, $device_total)
-						$stdout_buffer[1] = $stdout_bytes_read
-						$device.Write($stdout_buffer, 0, $M+1)
-						$device.Flush()
-						$stdout_task = $stdout.BeginRead($stdout_buffer, 2, ($M-1), $null, $null)
-					} else { # process closed?
-						$stdout_task = $null
-					}
-				} else {
-					[System.Console]::Write("x")
+			if ($stdout_task.IsCompleted -and $can_write) {
+				$stdout_bytes_read = $stdout.EndRead($stdout_task)
+				if ($stdout_bytes_read -gt 0) {
+					$stdout_total += $stdout_bytes_read
+					$stdout_buffer[1] = $stdout_bytes_read
+					$device.Write($stdout_buffer, 0, $M+1)
+					$device.Flush()
+					$stdout_task = $stdout.BeginRead($stdout_buffer, 2, ($M-1), $null, $null)
+				} else { # process closed?
+					$stdout_task = $null
 				}
-			} if ($device_task.IsCompleted) {
+				$io = $true
+			}
+			if ($device_task.IsCompleted) {
 				$device_bytes_read = $device.EndRead($device_task)
 				if ($device_bytes_read -gt 0) {
 					$can_write = (($device_buffer[1] -band 128) -ne 128)
 					$device_buffer[1] = $device_buffer[1] -band ($M-1)
-					$device_total += $device_buffer[1]
-					[string]::Format( "Socket {0} - Device {1}", $stdout_total, $device_total)
-					$stdin.Write($device_buffer, 2, $device_buffer[1])
-					$stdin.Flush()
+					if ($device_buffer[1] -gt 0) {
+						$device_total += $device_buffer[1]
+						$stdin.Write($device_buffer, 2, $device_buffer[1])
+						$stdin.Flush()
+					}
 					$device_task = $device.BeginRead($device_buffer, 0, ($M+1), $null, $null)
 				} else {
 					$device_task = $null
 				}
 				$device.Write($empty_buffer, 0, $M+1)
-			} else {
+				$io = $true
+			}
+			if (!$io) {
 				Start-Sleep -m 1
 			}
 		} catch {
