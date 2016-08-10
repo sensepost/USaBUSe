908630d735411290f3c94de04e2793fb67df1c0c
Rogan Dawes <rogan@sensepost.com>
2016-08-10 16:00:07 +0200

    Update avr code to current version of LUFA

--- avr/KeyboardMouseGeneric/Descriptors.h
+++ avr/KeyboardMouseGeneric/Descriptors.h
@@ -114,7 +114,7 @@ void dsc_initDebugBuffer(RingBuffer_t *buffer);
 
 	/* Function Prototypes: */
 		uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue,
-		                                    const uint8_t wIndex,
+		                                    const uint16_t wIndex,
 		                                    const void** const DescriptorAddress)
 		                                    ATTR_WARN_UNUSED_RESULT ATTR_NON_NULL_PTR_ARG(3);
 
